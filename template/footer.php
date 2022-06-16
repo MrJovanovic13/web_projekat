f02eb92b5e62c633e0319cd01450c8a801d2e027
MrJovanovic13 <RespectNForever@gmail.com>
2022-06-16 04:30:21 +0200

    footer now populated with logos

--- template/footer.php
+++ template/footer.php
@@ -5,22 +5,20 @@
 
 	<div class="footer-right">
 
-		<a href="#"><i class="fa fa-facebook">
-				<img class="fb" src="../images/fb.png" alt="fb test"></i></a>
-		<a href="#"><i class="fa fa-twitter"></i></a>
-		<a href="#"><i class="fa fa-linkedin"></i></a>
-		<a href="#"><i class="fa fa-github"></i></a>
+		<a href="#"><i class="fa fa-facebook"><img class="fb" src="../images/fb.png" alt="fb"></i></a>
+		<a href="#"><i class="fa fa-insta"><img class="fb" src="../images/insta.png" alt="insta"></i></a>
+		<a href="#"><i class="fa fa-linkedin"><img class="fb" src="../images/linkedin.png" alt="linkedin"></i></a>
+		<a href="#"><i class="fa fa-twitter"><img class="fb" src="../images/twitter.png" alt="twitter"></i></a>
 
 	</div>
 
 	<div class="footer-left">
 
 		<p class="footer-links">
-			<a class="link-1" href="#">Home</a>
+			<a class="link-1" href="../home/">Home </a>
+			<a class="link-1" href="../products/">Products </a>
+			<a class="link-1" href="../my-account/">My account </a>
 
-			<a href="#">Products</a>
-
-			<a href="#">About us</a>
 		</p>
 
 	</div>
